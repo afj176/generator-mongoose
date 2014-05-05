69ec44367bb9051567f17eab8f4748b354f1ab3f
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-05-05 12:57:23 +0530

    Update the code base as per expressjs 4.0

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -3,57 +3,70 @@
 // Module dependencies.
 var express = require('express'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    methodOverride = require('method-override'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    errorhandler = require('errorhandler');
 
 var app = module.exports = exports.app = express();
 
-app.locals.siteName = "<%= _.capitalize(appname) %>";
+app.locals.siteName = "MongooseGettingStarted";
 
 // Connect to database
 var db = require('./config/db');
 
+app.use(express.static(__dirname + '/public'));
+
 // Bootstrap models
 var modelsPath = path.join(__dirname, 'models');
 fs.readdirSync(modelsPath).forEach(function (file) {
-  require(modelsPath + '/' + file);
+    require(modelsPath + '/' + file);
 });
 
-app.configure('development', function() {
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler({ 
-    dumpExceptions: true , showStack: true
-  }));
-  app.set('view options', { pretty: true });
-});
+var env = process.env.NODE_ENV || 'development';
 
-app.configure('test', function() {
-  app.use(express.logger('test'));
-  app.set('view options', { pretty: true });
-});
+if ('development' == env) {
+    app.use(morgan('dev'));
+    app.use(errorhandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.set('view options', {
+        pretty: true
+    });
+}
 
-app.configure('production', function() {
-  app.use(express.logger());
-  app.use(express.errorHandler());
-});
 
-app.configure(function(){
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
-	app.use(express.methodOverride());
-  app.use(express.urlencoded());
-  app.use(express.json());
-  // Router needs to be last
-	app.use(app.router);
-});
+if ('test' == env) {
+    app.use(morgan('test'));
+    app.set('view options', {
+        pretty: true
+    });
+}
+
+if ('production' == env) {
+    app.use(morgan());
+    app.use(express.errorHandler());
+}
+
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(methodOverride());
+app.use(bodyParser());
+// Router needs to be last
+//app.use(app.router);
 
 // Bootstrap routes/api
 var routesPath = path.join(__dirname, 'routes');
-fs.readdirSync(routesPath).forEach(function(file) {
-  require(routesPath + '/' + file)(app);
+fs.readdirSync(routesPath).forEach(function (file) {
+    require(routesPath + '/' + file)(app);
 });
 
 // Start server
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+    console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
